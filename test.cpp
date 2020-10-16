4d68e3593971c65a43c904d77b730e06e6a5ecba
Rohan <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2020-10-16 20:42:47 +0530

    code

--- test.cpp
+++ test.cpp
@@ -11,16 +11,11 @@ using namespace std::chrono;
 int minJumps(int arr[], int n)
 {
 
-	// Base case: when source and
-	// destination are same
+
 	if (n == 1)
 		return 0;
 
-	// Traverse through all the points
-	// reachable from arr[l]
-	// Recursivel, get the minimum number
-	// of jumps needed to reach arr[h] from
-	// these reachable points
+
 	int res = INT_MAX;
 	for (int i = n - 2; i >= 0; i--) {
 		if (i + arr[i] >= n - 1) {
@@ -38,18 +33,26 @@ int minJumps(int arr[], int n)
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		cout << minJumps(a, n) << endl;
 
 
-	int arr[] = { 1, 3, 6, 3, 2,
-	              3, 6, 8, 9, 5
-	            };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Minimum number of jumps to";
-	cout << " reach the end is " << minJumps(arr, n);
+	}
 	return 0;
 }
 
